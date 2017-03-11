e6a73cd4dfdf93beb178b4f3d73701d5e7d4a4ba
Hellsflash <hellsflash@gmail.com>
2017-03-12 00:20:22 +0200

    Solved Problems from PHP-Syntax Basic Web

---
+++ 16.Draw an S with Buttons.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>16. Draw an S with Buttons.</title>
+</head>
+<body>
+<?php
+for ($row = 0; $row < 9; $row++) {
+    for ($col = 0; $col < 5; $col++) {
+        if ($row == 0 || $row == 8 || $row == 4 || $col == 0 &&
+            ($row > 0 && $row < 5) || $col == 4 && $row > 4)
+        {
+            $color = "style='background-color:blue'";
+            $button = "<button $color>1</button>";
+        }
+        else
+        {
+            $button = "<button>0</button>";
+        }
+        echo $button;
+    }
+    echo "<br/>";
+}
+?>
+</body>
+</html>
